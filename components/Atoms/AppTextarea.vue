1bc77bc68c2bf8ebb75a87e6585368e55e3f4120
seee-shun <seino.shungo.17@shizuoka.ac.jp>
2020-09-18 11:04:18 +0900

    prettier修正

---
+++ components/Atoms/AppTextarea.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="textarea-place">
+    <textarea
+      color="#61d4b3"
+      class="textarea-text"
+      :value="textareaValue"
+      :placeholder="textareaPlaceholder"
+      @input="$emit('input', $event.target.value)"
+    ></textarea>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    textareaValue: {
+      type: String,
+      required: true,
+      default: '',
+    },
+    textareaPlaceholder: {
+      type: String,
+      required: true,
+      default: '',
+    },
+  },
+};
+</script>
+<style scoped>
+.textarea-place {
+  text-align: center;
+}
+.textarea-text {
+  border: 1px solid #61d4b3;
+  width: 300px;
+  outline: none;
+}
+</style>
