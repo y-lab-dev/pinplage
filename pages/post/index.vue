4c400abc16cd7c026e82f18d9044bf7dfb70d8fa
seee-shun <seino.shungo.17@shizuoka.ac.jp>
2020-09-21 01:45:45 +0900

    投稿の枠組み

--- pages/post/index.vue
+++ pages/post/index.vue
@@ -1,10 +1,74 @@
 <template>
-  <div>
-    <nuxt-link to="post/threadPost">スレッド投稿</nuxt-link>
-    <nuxt-link to="post/wisdomPost">知恵袋投稿</nuxt-link>
-    <nuxt-link to="post/eventPost">イベント投稿</nuxt-link>
-    <nuxt-link to="post/placePost">周辺施設投稿</nuxt-link>
-    <nuxt-link to="post/parttimeJobPost">アルバイト投稿</nuxt-link>
+  <div class="header-footer">
+    <v-container>
+      <v-row justify="center">
+        <v-col cols="6">
+          <nuxt-link to="post/threadPost" class="card-title">
+            <v-card>
+              <v-img
+                class="white--text align-end"
+                height="150px"
+                src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
+              >
+                <v-card-title>スレッド</v-card-title>
+              </v-img>
+            </v-card>
+          </nuxt-link>
+        </v-col>
+        <v-col cols="6">
+          <nuxt-link to="post/wisdomPost" class="card-title">
+            <v-card>
+              <v-img
+                class="white--text align-end"
+                height="150px"
+                src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
+              >
+                <v-card-title>知恵袋</v-card-title>
+              </v-img>
+            </v-card>
+          </nuxt-link>
+        </v-col>
+        <v-col cols="12">
+          <nuxt-link to="post/eventPost" class="card-title">
+            <v-card>
+              <v-img
+                class="white--text align-end"
+                height="150px"
+                src="https://cdn.vuetifyjs.com/images/cards/cooking.png"
+              >
+                <v-card-title>イベント</v-card-title>
+              </v-img>
+            </v-card>
+          </nuxt-link>
+        </v-col>
+        <v-col cols="6">
+          <nuxt-link to="post/placePost" class="card-title">
+            <v-card>
+              <v-img
+                class="white--text align-end"
+                height="150px"
+                src="https://cdn.vuetifyjs.com/images/cards/road.jpg"
+              >
+                <v-card-title>周辺施設</v-card-title>
+              </v-img>
+            </v-card>
+          </nuxt-link>
+        </v-col>
+        <v-col cols="6">
+          <nuxt-link to="post/parttimeJobPost" class="card-title">
+            <v-card>
+              <v-img
+                class="white--text align-end"
+                height="150px"
+                src="https://cdn.vuetifyjs.com/images/cards/plane.jpg"
+              >
+                <v-card-title> アルバイト</v-card-title>
+              </v-img>
+            </v-card>
+          </nuxt-link>
+        </v-col>
+      </v-row>
+    </v-container>
   </div>
 </template>
 <script>
@@ -12,3 +76,12 @@ export default {
   layout: 'protected',
 };
 </script>
+<style scoped>
+.header-footer {
+  padding-top: 60px;
+  padding-bottom: 60px;
+}
+.card-title {
+  color: aliceblue;
+}
+</style>
