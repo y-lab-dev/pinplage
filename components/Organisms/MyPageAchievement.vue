4bb60ef14d2b3ba1ef969a0b53b48923b3896fb4
takahirokaji <45552770+takahirokaji@users.noreply.github.com>
2020-09-21 14:47:55 +0900

    壊れたヘッダーブランチから移植しました

---
+++ components/Organisms/MyPageAchievement.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <v-card class="mx-auto" max-height="9vh" :max-width="`${cardInfoTitleSize}vw`" elevation="1">
+      <v-container>
+        <v-row class="justify-center" no-gutters>
+          <v-col v-for="item in cardInfo" :key="item.name" cols="4">
+            <p class="ma-0 card-info-amount">{{ item.amount }}</p>
+            <p class="ma-0 card-info-title">{{ item.title }}</p>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-card>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      cardInfo: [{ title: 'ポイント', amount: '1231pp' }],
+    };
+  },
+  computed: {
+    cardInfoTitleSize() {
+      if (this.$vuetify.breakpoint.smAndDown) {
+        return 90;
+      } else {
+        return 50;
+      }
+    },
+  },
+};
+</script>
+<style scoped>
+.card-info-title {
+  font-size: 0.6rem;
+  color: rgba(0, 0, 0, 0.6);
+  text-align: center;
+}
+.card-info-amount {
+  font-size: 1rem;
+  color: rgba(14, 232, 68, 0.6);
+  text-align: center;
+}
+</style>
--- layouts/protected.vue
+++ layouts/protected.vue
@@ -1,5 +1,6 @@
 <template>
   <v-app>
+    <top-header />
     <v-main>
       <nuxt />
     </v-main>
@@ -9,10 +10,17 @@
   </v-app>
 </template>
 <script>
+import TopHeader from '~/components/Organisms/TopHeader';
 import BottomTab from '~/components/Organisms/TopBottom';
 export default {
   components: {
     BottomTab,
+    TopHeader,
   },
 };
 </script>
+<style scoped>
+#app {
+  font-family: 'Kosugi', sans-serif;
+}
+</style>
