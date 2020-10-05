3f614a801602e0083d660679ccb36536a42aadb9
seee-shun <seino.shungo.17@shizuoka.ac.jp>
2020-10-05 17:36:10 +0900

    募集中かどうかの判定機能

--- pages/timeline/job/detailJob.vue
+++ pages/timeline/job/detailJob.vue
@@ -107,6 +107,18 @@
         キープ済み
       </v-btn>
     </v-list-item>
+    <v-list-item v-show="!isClose">
+      <v-btn rounded color="pink" large dark @click="close">
+        <v-icon left>mdi-star</v-icon>
+        募集を締め切る
+      </v-btn>
+    </v-list-item>
+    <v-list-item v-show="isClose">
+      <v-btn rounded color="grey" large dark>
+        <v-icon left>mdi-star</v-icon>
+        募集を終了しました
+      </v-btn>
+    </v-list-item>
     <v-list-item v-show="isEdit">
       <nuxt-link to="/timeline/job/jobEdit">
         <v-btn rounded color="red" large dark>
@@ -129,6 +141,7 @@ export default {
       jobDetailArray: [],
       isKeep: false,
       isEdit: false,
+      isClose: false,
     };
   },
   computed: {
@@ -161,6 +174,9 @@ export default {
       ];
       if (that.uid === doc.data().uid) {
         that.isEdit = true;
+      }
+      if (doc.data().isRecruit === false) {
+        that.isClose = true;
       }
     });
     jobDetail.get().then((snapshot) => {
@@ -222,6 +238,22 @@ export default {
         .then(() => {
           alert('キープを解除しました');
           that.isKeep = false;
+        })
+        .catch((err) => {
+          alert(err);
+        });
+    },
+    close() {
+      const that = this;
+      const job = firebase.firestore().collection('jobs').doc(this.id);
+
+      job
+        .update({
+          isRecruit: false,
+        })
+        .then(() => {
+          alert('このアルバイトの募集を締め切りました');
+          that.isClose = true;
         })
         .catch((err) => {
           alert(err);
