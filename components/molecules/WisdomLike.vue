e0580878fed906bba4fc2698f203b226d97d8d97
takahirokaji <45552770+takahirokaji@users.noreply.github.com>
2020-10-19 01:00:07 +0900

    投稿本体へのいいね機能の実装

--- components/molecules/WisdomLike.vue
+++ components/molecules/WisdomLike.vue
@@ -1,3 +1,67 @@
 <template>
-  <v-icon>mdi-heart-outline</v-icon>
+  <v-icon dark small :color="alreadyLiked ? '#f00' : '#E6E6E6'" @click="addWisdomLike"
+    >mdi-heart</v-icon
+  >
 </template>
+
+<script>
+import { mapGetters } from 'vuex';
+import firebase from '~/plugins/firebase';
+export default {
+  props: {
+    wisdomId: {
+      required: true,
+      type: String,
+    },
+  },
+  data() {
+    return {
+      alreadyLiked: '',
+    };
+  },
+  computed: {
+    ...mapGetters({
+      uid: 'user/uid',
+      likedWisdoms: 'user/likedPost',
+    }),
+  },
+  created() {
+    this.alreadyLiked = this.likedWisdoms.some((value) => {
+      return value === this.wisdomId;
+    });
+  },
+  methods: {
+    addWisdomLike() {
+      const that = this;
+      const userWisdomLikedPost = firebase
+        .firestore()
+        .collection('users')
+        .doc(this.uid)
+        .collection('wisdom')
+        .doc('likedPost');
+      if (!this.alreadyLiked) {
+        userWisdomLikedPost
+          .set(
+            {
+              id: firebase.firestore.FieldValue.arrayUnion(that.wisdomId),
+            },
+            { merge: true }
+          )
+          .then(() => {
+            that.alreadyLiked = true;
+            that.$store.dispatch('user/getUserWisdom');
+          });
+      } else {
+        userWisdomLikedPost
+          .update({
+            id: firebase.firestore.FieldValue.arrayRemove(that.wisdomId),
+          })
+          .then(() => {
+            that.alreadyLiked = false;
+            that.$store.dispatch('user/getUserWisdom');
+          });
+      }
+    },
+  },
+};
+</script>
