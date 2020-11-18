491bd9ccd2c2c651e8ad9f0e03f78f2e808891ed
RyoJosaki <46699768+RyoJosaki@users.noreply.github.com>
2020-11-19 03:21:06 +0900

    クチコミ投稿

--- pages/post/reviewPost.vue
+++ pages/post/reviewPost.vue
@@ -1,153 +1,272 @@
 <template>
-  <v-container>
+  <div style="height: 100%" align="center">
     <div ref="map" />
-    <v-row justify="center">
-      <v-col cols="12" md="8" sm="6">
-        <v-card>
-          <v-card-text>
-            <h2 class="center">クチコミ</h2>
-            <input-place :label="placeholder" @place="placeAdd"></input-place>
-            <rating :star-size="50" @rating-selected="rating = $event" @input="star"></rating>
-            <br />
-            <div>
-              <dl>
-                <dd>
-                  <input id="scene0" v-model="scene" value="朝" type="checkbox" class="scene" />
-                  <label class="check_label" for="scene0">朝</label>
-                  <input id="scene1" v-model="scene" value="昼" type="checkbox" class="scene" />
-                  <label class="check_label" for="scene1">昼</label>
-                  <input id="scene2" v-model="scene" value="夜" type="checkbox" class="scene" />
-                  <label class="check_label" for="scene2">夜</label>
-                  <input id="scene3" v-model="scene" value="その他" type="checkbox" class="scene" />
-                  <label class="check_label" for="scene3">その他</label>
-                </dd>
-              </dl>
-            </div>
-            <div class="cb"></div>
-            <input-image :img-path="imgPath" @imgSubmit="imgAdd"></input-image>
-            <v-textarea
-              v-model="content"
-              color="#61d4b3"
-              auto-grow
-              label="コメント"
-              rows="5"
-              prepend-icon="mdi-message-processing"
-            ></v-textarea>
-            <cool-select
-              v-model="selected"
-              class="tags"
-              :items="hashtags"
-              placeholder="#ハッシュタグ"
-            />
-            <v-icon class="float">mdi-cash-multiple</v-icon>
-            <v-textarea
-              v-model="budgetLow"
-              color="#61d4b3"
-              auto-grow
-              label="最低予算"
-              rows="1"
-              class="budget"
-            ></v-textarea>
-            <h3 class="float">円～&nbsp;</h3>
-            <v-textarea
-              v-model="budgetHigh"
-              color="#61d4b3"
-              auto-grow
-              label="最高予算"
-              rows="1"
-              class="budget"
-            ></v-textarea>
-            <h3 class="float">円&nbsp;</h3>
-            <div class="cb"></div>
-            <v-textarea
-              v-model="genre"
-              color="#61d4b3"
-              auto-grow
-              label="ジャンル (ラーメン,居酒屋,etc)"
-              rows="1"
-              prepend-icon="mdi-pencil"
-            ></v-textarea>
-            <div class="center">
-              <!-- <post-button>投稿</post-button> -->
-              <post-button
-                class="center"
-                :button-method="post"
-                :button-type="buttonType"
-                :button-disabled="rating == 0 || placeName == '' || content == ''"
-                >投稿</post-button
-              >
-            </div>
-          </v-card-text>
-        </v-card>
-      </v-col>
-    </v-row>
-  </v-container>
+    <v-card width="98vw">
+      <v-container style="height: 100%" fluid>
+        <v-row justify="center" align-content="center" style="height: 100%" fluid>
+          <v-col>
+            <v-row justify="center" align-content="center">
+              <v-col align-content="center">
+                <h3 align="center">クチコミ</h3>
+                <v-card-subtitle align="center">※は必須項目です</v-card-subtitle>
+                <input-place :label="placeholder" @place="placeAdd"></input-place>
+                <v-row justify="center" align="center">
+                  <v-col cols="4">※おすすめ度</v-col>
+                  <v-col cols="8">
+                    <rating
+                      :star-size="30"
+                      @rating-selected="rating = $event"
+                      @input="star"
+                    ></rating>
+                  </v-col>
+                </v-row>
+                <v-row justify="center" align-content="center">
+                  <v-col cols="5">利用した時間帯</v-col>
+                  <v-col cols="7" class="pt-0">
+                    <dl>
+                      <dd>
+                        <v-row class="pa-0">
+                          <v-col cols="4" align="center" class="pa-0">
+                            <input
+                              id="scene0"
+                              v-model="scene"
+                              value="朝"
+                              type="radio"
+                              class="scene"
+                              @click="radioDeselection(this, 1)"
+                            />
+                            <label class="check_label" for="scene0">朝</label>
+                          </v-col>
+                          <v-col cols="4" align="center" class="pa-0">
+                            <input
+                              id="scene1"
+                              v-model="scene"
+                              value="昼"
+                              type="radio"
+                              class="scene"
+                              @click="radioDeselection(this, 2)"
+                            />
+                            <label class="check_label" for="scene1">昼</label>
+                          </v-col>
+                          <v-col cols="1" align="center" class="pa-0">
+                            <input
+                              id="scene2"
+                              v-model="scene"
+                              value="夜"
+                              type="radio"
+                              class="scene"
+                              @click="radioDeselection(this, 3)"
+                            />
+                            <label class="check_label" for="scene2">夜</label>
+                          </v-col>
+                        </v-row>
+                      </dd>
+                    </dl>
+                  </v-col>
+                </v-row>
+                <input-image
+                  :img-path="imgPath"
+                  :lavel="imgLavel"
+                  :required-text="imgText"
+                  @imgSubmit="imgAdd"
+                ></input-image>
+                <v-textarea
+                  v-model="content"
+                  color="#61d4b3"
+                  auto-grow
+                  label="コメント"
+                  rows="5"
+                  prepend-icon="mdi-message-processing"
+                ></v-textarea>
+                <cool-select
+                  v-model="selected"
+                  class="tags"
+                  :items="hashtags"
+                  placeholder="#ハッシュタグ"
+                />
+                <v-row>
+                  <v-col>
+                    <v-icon class="float">mdi-cash-multiple</v-icon>
+                    <v-textarea
+                      v-model="budget"
+                      type="number"
+                      color="#61d4b3"
+                      auto-grow
+                      maxlength="6"
+                      label="使用した金額"
+                      rows="1"
+                      class="budget"
+                      oninput="value = value.replace(/[^0-9]+/i,'');"
+                    ></v-textarea>
+                    <h3 class="float">円</h3>
+                    <div class="cb"></div>
+                  </v-col>
+                </v-row>
+                <v-row align="center">
+                  <v-col cols="6">
+                    <v-textarea
+                      v-model="genre"
+                      color="#61d4b3"
+                      auto-grow
+                      label="ジャンル"
+                      rows="1"
+                      prepend-icon="mdi-pencil"
+                    ></v-textarea>
+                  </v-col>
+                  <v-col cols="4">
+                    <cool-select v-model="selectGenre" :items="genres" placeholder="選ぶ" />
+                  </v-col>
+                </v-row>
+                <v-row align="center">
+                  <v-col cols="12" align="center">
+                    <v-dialog v-model="postDialog" persistent max-width="290">
+                      <template v-slot:activator="{ on, attrs }">
+                        <div style="padding: 10px 9px">
+                          <v-btn
+                            rounded
+                            width="80vw"
+                            class="white--text button-color center"
+                            outlined
+                            v-bind="attrs"
+                            :type="buttonType"
+                            :disabled="rating == 0 || placeName == ''"
+                            v-on="on"
+                            >投稿</v-btn
+                          >
+                        </div>
+                      </template>
+                      <v-card>
+                        <v-card-title>投稿しますか？</v-card-title>
+                        <v-card-actions>
+                          <v-spacer></v-spacer>
+                          <v-btn color="pink darken-1" text @click="postDialog = false"
+                            >キャンセル</v-btn
+                          >
+                          <v-btn class="white--text button-color" tile @click="post()"
+                            >投稿する</v-btn
+                          >
+                        </v-card-actions>
+                      </v-card>
+                    </v-dialog>
+                    <v-dialog v-model="finishedDialog" persistent max-width="290">
+                      <v-card>
+                        <v-card-title>投稿しました。</v-card-title>
+                        <v-card-actions>
+                          <v-spacer></v-spacer>
+                          <v-btn color="pink darken-1" text @click="finish()">閉じる</v-btn>
+                        </v-card-actions>
+                      </v-card>
+                    </v-dialog>
+                  </v-col>
+                </v-row>
+              </v-col>
+            </v-row>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-card>
+  </div>
 </template>
 
 <script>
 import { mapGetters } from 'vuex';
 import { CoolSelect } from 'vue-cool-select';
-import algoliasearch from 'algoliasearch';
-import PostButton from '~/components/Atoms/AppButton';
 import Rating from '~/components/molecules/PostRating';
 import InputImage from '~/components/Molecules/AppImageInput';
 import InputPlace from '~/components/Molecules/AppInputPlace';
 import firebase from '~/plugins/firebase';
 
-const reviewHashtags = firebase.firestore().collection('reviewHashtags');
+const reviewTags = firebase.firestore().collection('reviewTags');
 
 export default {
-  layout: 'protected',
+  layout: 'onlyBack',
   components: {
     CoolSelect,
-    PostButton,
     Rating,
     InputImage,
     InputPlace,
   },
   data() {
     return {
-      searchClient: algoliasearch('ZI3A3BYLFT', 'dd660cd8b43da0cc68310ac6a365438b'),
-      placeholder: '店名',
+      placeholder: '※場所を入力',
       inputType: 'text',
       buttonType: 'submit',
       contentPlaceholder: '内容',
       placeId: '',
-      placeName: '',
       mainImg: '',
+      img: '',
       subImg: [],
       content: '',
       rating: 0,
-      budgetLow: null,
-      budgetHigh: null,
+      budget: null,
       scene: [],
       hashtags: [],
+      genres: [],
+      selectGenre: '',
       genre: '',
       keywords: [],
       postValidation: false,
       fiels: ['place_id', 'name', 'type'],
       imgPath: 'reviews/image/',
+      imgLavel: '画像',
+      imgText: '画像が選択されていません',
       selected: null,
+      remove: 0,
+      postDialog: false,
+      finishedDialog: false,
     };
   },
   computed: {
-    ...mapGetters({ uid: 'user/uid', email: 'user/email' }),
+    ...mapGetters({
+      uid: 'user/uid',
+      email: 'user/email',
+      id: 'place/id',
+      pname: 'place/pname',
+    }),
   },
   watch: {
     selected() {
       if (this.selected !== null) {
-        this.content = this.content + '\n' + this.selected;
-        this.selected = '';
+        if (this.selected !== '') {
+          this.content = this.content + '\n' + this.selected;
+          this.selected = '';
+        }
+      }
+    },
+    selectGenre() {
+      if (this.selectGenre !== null) {
+        if (this.selectGenre !== '') {
+          this.genre = this.selectGenre;
+          this.selectGenre = null;
+        }
       }
     },
   },
+  created() {
+    if (this.id) {
+      this.placeId = this.id;
+      this.placeName = this.pname;
+      this.placeholder = this.pname;
+      this.$store.commit('place/getName', '');
+    }
+  },
   mounted() {
     const self = this;
-    reviewHashtags
-      .doc('browse')
+    reviewTags
+      .doc('hashtags')
       .get()
       .then((doc) => {
         self.hashtags = doc.data().hashtags;
+      })
+      .catch((err) => {
+        alert(err);
+        console.log(err);
+      });
+    reviewTags
+      .doc('genres')
+      .get()
+      .then((doc) => {
+        self.genres = doc.data().genres;
       })
       .catch((err) => {
         alert(err);
@@ -159,7 +278,6 @@ export default {
       const place = this.mapAutoComplete.getPlace();
       this.placeId = place.place_id;
       this.placeName = place.name;
-      console.log(place);
     },
     star(star) {
       this.rating = star;
@@ -171,15 +289,43 @@ export default {
       this.placeId = val.placeId;
       this.placeName = val.placeName;
     },
+    radioDeselection(already, numeric) {
+      if (this.remove === numeric) {
+        this.scene = '';
+        this.remove = 0;
+      } else {
+        this.remove = numeric;
+      }
+    },
     post() {
       const self = this;
-      const str = this.content.replace('＃', '#');
-      const tags = str.match(/[#＃][Ａ-Ｚａ-ｚA-Za-z一-鿆0-9０-９ぁ-ヶｦ-ﾟー._-]+/gm);
-      const arr = this.hashtags.concat(tags);
-      this.hashtags = [...new Set(arr)];
-      reviewHashtags.doc('browse').update({
-        hashtags: self.hashtags,
-      });
+      let str = '';
+      let tags = [];
+      if (this.content !== '') {
+        str = this.content.replace('＃', '#');
+        console.log(str);
+        tags = str.match(/[#＃][Ａ-Ｚａ-ｚA-Za-z一-鿆0-9０-９ぁ-ヶｦ-ﾟー._-]+/gm);
+        if (tags) {
+          const tags2 = tags.filter((n) => n !== '#');
+          console.log(tags2);
+          const arr = this.hashtags.concat(tags2);
+          console.log(arr);
+          let newHashtags = this.hashtags;
+          newHashtags = [...new Set(arr)];
+          if (this.hashtags !== newHashtags) {
+            reviewTags.doc('hashtags').update({
+              hashtags: newHashtags,
+            });
+          }
+        }
+      }
+      if (this.genre !== '') {
+        const arr2 = this.genres.concat(this.genre);
+        this.genres = [...new Set(arr2)];
+        reviewTags.doc('genres').update({
+          genres: self.genres,
+        });
+      }
 
       const review = firebase.firestore().collection('reviews');
       const timestamp = firebase.firestore.Timestamp.now();
@@ -187,8 +333,8 @@ export default {
         .add({
           placeId: self.placeId,
           name: self.placeName,
-          mainImgUrl: self.mainImg,
-          comment: self.content,
+          mainImgUrl: self.img,
+          comment: str,
           rating: self.rating,
           scene: self.scene,
           hashtags: tags,
@@ -204,15 +350,13 @@ export default {
             .collection('detail')
             .doc('browse')
             .set({
-              budgetLow: self.budgetLow,
-              budgetHigh: self.budgetHigh,
+              budget: self.budget,
               imgUrl: self.subImg,
               updatedAt: timestamp,
             })
             .then(() => {
-              // self.$router.push({ name: 'timeline' });
-              alert('コメントが投稿されました');
-              this.$router.go(-1);
+              self.postDialog = false;
+              self.finishedDialog = true;
             })
             .catch((err) => {
               alert(err);
@@ -222,20 +366,20 @@ export default {
           alert(err);
         });
     },
+    finish() {
+      this.finishedDialog = false;
+      this.$router.push({ name: 'timeline' });
+    },
   },
 };
 </script>
 
 <style scoped>
-.center {
-  text-align: center;
-}
-
-input[type='checkbox'] {
+input[type='radio'] {
   display: none;
 }
-input[type='checkbox'].scene:checked + label {
-  background: #61d4b3;
+input[type='radio'].scene:checked + label {
+  background: orange;
   color: #fff;
 }
 .check_label {
@@ -249,8 +393,8 @@ input[type='checkbox'].scene:checked + label {
   padding-left: 5px;
   padding-right: 5px;
   cursor: pointer;
-  color: #61d4b3;
-  border: 2px solid #61d4b3;
+  color: gray;
+  border: 2px solid grey;
   border-radius: 5px;
 }
 .cb {
@@ -263,13 +407,17 @@ input[type='checkbox'].scene:checked + label {
 }
 .budget {
   float: left;
-  width: 72px;
+  width: 120px;
 }
 
 .tags {
   box-shadow: none !important;
   width: 180px;
   padding: 2px 2px;
+}
+.button-color {
+  background: linear-gradient(to right, #36aa37 0%, #2da8aa 50%);
+  opacity: 0.6;
 }
 </style>
 <style>
