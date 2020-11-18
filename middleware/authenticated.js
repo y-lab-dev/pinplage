1a75a30eefaca115f5d47c157c3e91f79ad18fd9
seee-shun <seino.shungo.17@shizuoka.ac.jp>
2020-11-19 05:01:05 +0900

    pwa化成功だけど不安あり

--- middleware/authenticated.js
+++ middleware/authenticated.js
@@ -2,7 +2,6 @@ export default function ({ store, redirect }) {
   if (!store.getters['user/isAuthenticated']) {
     return redirect('/login');
   } else {
-    store.dispatch('user/getUserInfo');
     return redirect('/timeline');
   }
 }
