22d5df610588683894c320931a86b8d4e3388a73
seee-shun <seino.shungo.17@shizuoka.ac.jp>
2020-11-21 00:31:20 +0900

    middlewareのレンダリングうまくいかないが

--- store/index.js
+++ store/index.js
@@ -4,10 +4,10 @@ export const actions = {
   async nuxtServerInit({ commit, dispatch }, { req }) {
     const user = getUserFromCookie(req);
     if (user) {
+      await commit('user/switchLogin');
       const { user_id: uid, email } = user;
       const obj = { uid, email };
       await commit('user/getData', obj);
-      await commit('user/switchLogin');
       await dispatch('user/getUserInfo');
     }
   },
