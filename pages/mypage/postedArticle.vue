155955dc6a27c93a20fbf8331e7955237b8f6fe3
takahirokaji <45552770+takahirokaji@users.noreply.github.com>
2020-11-21 09:53:40 +0900

    学生記事、投稿したコメント修正

--- pages/mypage/postedArticle.vue
+++ pages/mypage/postedArticle.vue
@@ -49,7 +49,6 @@ export default {
         .orderBy('createdAt', 'desc')
         .get()
         .then((snapshot) => {
-          console.log();
           snapshot.forEach((doc) => {
             if (doc.ref.parent.parent.parent.id === 'reviews') {
               return;
