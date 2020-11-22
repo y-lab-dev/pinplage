73978e5e47fa59a083c5f9b7640faadcc1152f62
seee-shun <seino.shungo.17@shizuoka.ac.jp>
2020-11-22 22:49:03 +0900

    カラー変更

--- components/molecules/AppModal.vue
+++ components/molecules/AppModal.vue
@@ -13,7 +13,7 @@
 
         <v-card-actions>
           <v-spacer></v-spacer>
-          <v-btn color="red" text @click="changeModalValue()">
+          <v-btn color="primary" text @click="changeModalValue()">
             {{ modalButton }}
           </v-btn>
         </v-card-actions>
