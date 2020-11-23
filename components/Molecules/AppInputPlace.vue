02ecf617b8ed8ef0b8f30c2fb7150d963df81bcd
takahirokaji <45552770+takahirokaji@users.noreply.github.com>
2020-11-23 19:59:56 +0900

    Folder Name molecules ⇒ Molecules

---
+++ components/Molecules/AppInputPlace.vue
@@ -0,0 +1,103 @@
+<template>
+  <div>
+    <div ref="map" />
+    <v-text-field
+      ref="input"
+      v-model="placeName"
+      color="#61d4b3"
+      :label="label"
+      rows="1"
+      prepend-icon="mdi-map-marker-radius"
+      :rules="[() => !!placeName || requiredText]"
+      required
+    ></v-text-field>
+  </div>
+</template>
+
+<script>
+import loadGoogleMapsApi from 'load-google-maps-api';
+
+async function initMap() {
+  const gmap = await loadGoogleMapsApi({
+    key: 'AIzaSyCkPkussjC7YNEMi8dY9jwWy-XXZK9-SmA',
+    libraries: ['places'],
+    language: 'ja',
+  });
+  return gmap;
+}
+export default {
+  props: {
+    label: {
+      type: String,
+      required: true,
+      default: '',
+    },
+  },
+  data() {
+    return {
+      gmap: {},
+      mapAutoComplete: null,
+      map: null,
+      coord: { lat: 34.709557, lng: 137.726014 },
+      defaultZoom: 14.0,
+      restriction: {
+        latLngBounds: {
+          north: 34.854409,
+          south: 34.38496,
+          west: 137.547374,
+          east: 137.820871,
+        },
+        strictBounds: false,
+      },
+      fiels: ['place_id', 'name', 'type', 'geometry'],
+      placeName: '',
+      requiredText: 'この項目は必須です',
+    };
+  },
+  async mounted() {
+    this.gmap = await initMap();
+    console.log(this.$refs);
+
+    this.map = new this.gmap.Map(this.$refs.map, {
+      center: new this.gmap.LatLng(this.coord.lat, this.coord.lng),
+      zoom: this.defaultZoom,
+      restriction: this.restriction,
+      mapTypeControl: false,
+      streetViewControl: false,
+    });
+    const defaultBounds = new this.gmap.LatLngBounds(
+      new this.gmap.LatLng(this.restriction.latLngBounds.south, this.restriction.latLngBounds.west),
+      new this.gmap.LatLng(this.restriction.latLngBounds.north, this.restriction.latLngBounds.east)
+    );
+    const searchOptions = {
+      bounds: defaultBounds,
+      componentRestrictions: { country: 'jp' },
+      types: ['establishment'],
+      strictBounds: true,
+    };
+    let element = this.$refs.input.$el;
+    element = element.querySelector('input');
+    this.mapAutoComplete = new this.gmap.places.Autocomplete(element, searchOptions);
+    this.mapAutoComplete.setFields(this.fiels);
+    this.mapAutoComplete.addListener('place_changed', () => {
+      this.onClickLocation();
+    });
+  },
+
+  methods: {
+    onClickLocation() {
+      const place = this.mapAutoComplete.getPlace();
+      this.placeName = place.name;
+      const geometry = {
+        lat: place.geometry.location.lat(),
+        lng: place.geometry.location.lng(),
+      };
+      this.$emit('place', {
+        placeId: place.place_id,
+        placeName: place.name,
+        geometry,
+      });
+    },
+  },
+};
+</script>
---
+++ components/Molecules/PostRating.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <star-rating
+      :max-rating="5"
+      :show-rating="false"
+      :star-size="starSize"
+      @rating-selected="setRating"
+    ></star-rating>
+  </div>
+</template>
+
+<script>
+import StarRating from 'vue-star-rating';
+export default {
+  components: {
+    StarRating,
+  },
+  props: {
+    starSize: {
+      type: Number,
+      required: true,
+      default: 30,
+    },
+  },
+  data() {
+    return {
+      rating: 0,
+    };
+  },
+  watch: {
+    rating() {
+      this.$emit('input', this.rating);
+    },
+  },
+  methods: {
+    setRating(rating) {
+      this.rating = rating;
+    },
+  },
+};
+</script>
