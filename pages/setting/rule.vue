5c6efb74c0d0dc9a3719060f5fbd54c1661c521d
seee-shun <seino.shungo.17@shizuoka.ac.jp>
2020-11-26 02:45:34 +0900

    利用規約ページとログアウト機能作成

---
+++ pages/setting/rule.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col>
+        <AppRule class="app-rule" />
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+import AppRule from '~/components/Molecules/AppRule';
+export default {
+  layout: 'onlyBack',
+  components: {
+    AppRule,
+  },
+};
+</script>
+<style>
+.app-rule {
+  width: 80%;
+}
+</style>
