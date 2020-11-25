88cb027e7177abe00acee636b34b3ae2149cc0e0
seee-shun <seino.shungo.17@shizuoka.ac.jp>
2020-11-26 00:45:05 +0900

    ログインしてるかどうかの判定

--- middleware/autoLogin.js
+++ middleware/autoLogin.js
@@ -1,5 +1,7 @@
 export default function ({ store, redirect }) {
   if (!store.getters['user/isAuthenticated']) {
     return redirect('/login');
+  } else {
+    return redirect('/timeline');
   }
 }
