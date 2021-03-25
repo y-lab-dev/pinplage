55314e4eca8a4f55fc7edaa5f7af40c7123e6844
talahirokaji <tk69kj619@gmail.com>
2021-03-25 17:08:14 +0900

    left it to Vuex to get the data.

--- components/Organisms/TimelineWisdom.vue
+++ components/Organisms/TimelineWisdom.vue
@@ -1,14 +1,10 @@
 <template>
   <div>
-    <wisdom-thread
-      v-for="(item, index) in postedWisdoms"
-      :key="item.wisdomId"
-      v-bind="postedWisdoms[index]"
-    />
+    <wisdom-thread v-for="item in wisdoms" :key="item.wisdomId" v-bind="item" />
   </div>
 </template>
 <script>
-import firebase from '~/plugins/firebase';
+import { mapGetters } from 'vuex';
 import WisdomThread from '~/components/Organisms/WisdomThread';
 export default {
   components: { WisdomThread },
@@ -18,38 +14,15 @@ export default {
       likedWisdoms: '',
     };
   },
+  computed: {
+    ...mapGetters({
+      wisdoms: 'wisdom/wisdoms',
+    }),
+  },
   created() {
     this.$store.dispatch('user/getUserWisdom');
     this.$store.dispatch('user/getPostedWisdom');
     this.$store.dispatch('user/getUserLikedWisdomReply');
-    const that = this;
-    const wisdoms = firebase.firestore().collection('wisdoms');
-    wisdoms
-      .orderBy('createdAt', 'desc')
-      .where('college', '==', 'shizuoka-h')
-      .get()
-      .then((snapshot) => {
-        snapshot.forEach((doc) => {
-          const wisdom = doc.data();
-          const postedWisdom = {
-            wisdomId: doc.id,
-            poster: wisdom.poster,
-            resolved: wisdom.resolved,
-            likeAmount: wisdom.like,
-            content: wisdom.content,
-            category: wisdom.category,
-            createdAt: wisdom.createdAt.toDate(),
-          };
-          wisdoms
-            .doc(doc.id)
-            .collection('reply')
-            .get()
-            .then((snapshot) => {
-              postedWisdom.replyAmount = snapshot.size;
-              that.postedWisdoms.push(postedWisdom);
-            });
-        });
-      });
   },
 };
 </script>
