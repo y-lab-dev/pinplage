b6b3c728b4bc2a3346f4f1f548b8cac493e58278
talahirokaji <tk69kj619@gmail.com>
2021-05-28 18:32:34 +0900

    fix:部活・サークルのデータ取得処理をstoreに移動しました

--- pages/club/index.vue
+++ pages/club/index.vue
@@ -8,7 +8,7 @@
     <v-tabs-items v-model="clubTab" @change="pauseTab">
       <v-tab-item>
         <v-list>
-          <v-list-item v-for="(item, index) in club" :key="index" @click="clubDetail(item.id)">
+          <v-list-item v-for="(item, index) in clubs" :key="index" @click="clubDetail(item.id)">
             <v-list-item-avatar>
               <img :src="item.icon" />
             </v-list-item-avatar>
@@ -20,7 +20,7 @@
       </v-tab-item>
       <v-tab-item>
         <v-list>
-          <v-list-item v-for="(item, index) in circle" :key="index" @click="clubDetail(item.id)">
+          <v-list-item v-for="(item, index) in circles" :key="index" @click="clubDetail(item.id)">
             <v-list-item-avatar>
               <img :src="item.icon" />
             </v-list-item-avatar>
@@ -36,49 +36,26 @@
 
 <script>
 import { mapGetters } from 'vuex';
-import firebase from '~/plugins/firebase';
 export default {
   layout: 'protected',
+  fetch() {
+    this.$store.dispatch('club/getClubs');
+    this.$store.dispatch('club/getCircles');
+  },
   data() {
     return {
       clubTab: '',
-      club: [],
-      circle: [],
     };
   },
   computed: {
     ...mapGetters({
       tab: 'club/tab',
+      circles: 'club/circles',
+      clubs: 'club/clubs',
     }),
   },
   created() {
     this.clubTab = this.tab;
-    const that = this;
-    // const circles =
-    firebase
-      .firestore()
-      .collection('circles')
-      .orderBy('name')
-      .get()
-      .then((snapshot) => {
-        snapshot.forEach((doc) => {
-          if (doc.data().genre === '部活') {
-            const clubData = {
-              id: doc.id,
-              name: doc.data().name,
-              icon: doc.data().icon,
-            };
-            that.club.push(clubData);
-          } else {
-            const circleData = {
-              id: doc.id,
-              name: doc.data().name,
-              icon: doc.data().icon,
-            };
-            that.circle.push(circleData);
-          }
-        });
-      });
   },
   methods: {
     clubDetail(clubId) {
--- store/club.js
+++ store/club.js
@@ -1,15 +1,78 @@
+import firebase from '~/plugins/firebase';
+const db = firebase.firestore().collection('circles');
+
 export const state = () => ({
   clubTab: 'tab-1',
+  circles: [],
+  clubs: [],
+  circlesSize: 0,
+  clubsSize: 0,
 });
 
 export const getters = {
   tab(state) {
     return state.clubTab;
   },
+  circles(state) {
+    return state.circles;
+  },
+  clubs(state) {
+    return state.clubs;
+  },
 };
 
 export const mutations = {
   pauseTab(state, latestTab) {
     state.clubTab = latestTab;
+  },
+  setClubs(state, { club }) {
+    state.clubs = [...state.clubs, club];
+  },
+  setCircles(state, { circle }) {
+    state.circles = [...state.circles, circle];
+  },
+  setCirclesSize(state, { circlesSize }) {
+    state.circlesSize = circlesSize;
+  },
+  setClubsSize(state, { clubsSize }) {
+    state.circlesSize = clubsSize;
+  },
+};
+
+export const actions = {
+  getClubs({ state, commit }) {
+    if (state.clubsSize !== 0) return;
+    db.where('genre', '==', '部活')
+      .orderBy('name')
+      .get()
+      .then((snapshot) => {
+        commit('setClubsSize', { clubsSize: snapshot.size });
+        snapshot.forEach((doc) => {
+          const club = {
+            id: doc.id,
+            name: doc.data().name,
+            icon: doc.data().icon,
+          };
+          commit('setClubs', { club });
+        });
+      });
+  },
+  getCircles({ state, commit }) {
+    if (state.circlesSize !== 0) return;
+    db.where('genre', '==', 'サークル')
+      .orderBy('name')
+      .get()
+      .then((snapshot) => {
+        commit('setCirclesSize', { circlesSize: snapshot.size });
+        console.log(snapshot.size);
+        snapshot.forEach((doc) => {
+          const circle = {
+            id: doc.id,
+            name: doc.data().name,
+            icon: doc.data().icon,
+          };
+          commit('setCircles', { circle });
+        });
+      });
   },
 };
